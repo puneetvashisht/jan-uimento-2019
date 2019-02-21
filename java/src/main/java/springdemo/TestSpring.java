45d2107b9dc38a5de2089ea983df96296f023019
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2019-02-21 14:34:09 +0530

    testing

---
+++ java/src/main/java/springdemo/TestSpring.java
@@ -0,0 +1,31 @@
+package springdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:beans.xml")
+public class TestSpring {
+	
+	@Autowired
+	A a;
+	
+	@Autowired
+	B b;
+
+	@Test
+	public void test() {	
+		a.execute();
+		
+	}
+	
+	@Test
+	public void testB(){
+		b.execute();
+	}
+
+}
